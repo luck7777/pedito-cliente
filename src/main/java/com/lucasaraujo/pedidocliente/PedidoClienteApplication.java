ca5b51ffa6cc9e3d6c047c34f895993bc29f7ff6
Lucas Araujo <lucascaraujo@outlook.com>
2023-04-04 08:48:54 -0300

    Projeto criado

---
+++ src/main/java/com/lucasaraujo/pedidocliente/PedidoClienteApplication.java
@@ -0,0 +1,13 @@
+package com.lucasaraujo.pedidocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasaraujo/pedidocliente/PedidoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasaraujo.pedidocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
